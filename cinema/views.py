e956d4537215a2d01c1bea54c8e7830dfd013807
Dmitriy Kuzmin <dmitriykuzmin@ymail.com>
2026-03-24 19:50:37 +0200

    Solution

--- cinema/views.py
+++ cinema/views.py
@@ -1,8 +1,16 @@
 from datetime import datetime
 from typing import Type
 
-from django.db.models import F, Count, QuerySet
-from rest_framework import viewsets, mixins, status
+from django.db.models import (
+    F,
+    Count,
+    QuerySet
+)
+from rest_framework import (
+    viewsets,
+    mixins,
+    status
+)
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
@@ -10,9 +18,19 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.serializers import ModelSerializer
-from rest_framework.viewsets import GenericViewSet, ReadOnlyModelViewSet
+from rest_framework.viewsets import (
+    GenericViewSet,
+    ReadOnlyModelViewSet
+)
 
-from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession, Order
+from cinema.models import (
+    Genre,
+    Actor,
+    CinemaHall,
+    Movie,
+    MovieSession,
+    Order
+)
 from cinema.permissions import IsAdminOrIfAuthenticatedReadOnly
 from cinema.serializers import (
     GenreSerializer,
